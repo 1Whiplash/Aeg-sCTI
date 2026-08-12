95f0ec373912727ac5fba254277063702f580c6f
1Whiplash <alperkesici0@gmail.com>
2026-08-12 13:47:30 +0300

    Arama kutusuna IPv6 adres algilamasi ekle

--- frontend/src/lib/detectIocType.js
+++ frontend/src/lib/detectIocType.js
@@ -1,4 +1,7 @@
 const IPV4_REGEX = /^(?:\d{1,3}\.){3}\d{1,3}$/;
+// Standart tam/kısaltılmış IPv6 biçimlerinin (::1, fe80::1, 2001:db8::ff00:42:8329 vb.) hepsini kapsar.
+const IPV6_REGEX =
+  /^(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))$/;
 const HASH_REGEX = /^[a-fA-F0-9]{32}$|^[a-fA-F0-9]{40}$|^[a-fA-F0-9]{64}$/;
 const DOMAIN_REGEX = /^(?!-)[a-zA-Z0-9-]{1,63}(?:\.[a-zA-Z0-9-]{1,63})+$/;
 
@@ -11,6 +14,9 @@ export function detectIocType(rawValue) {
     return "url";
   }
   if (IPV4_REGEX.test(value) && value.split(".").every((octet) => Number(octet) <= 255)) {
+    return "ip";
+  }
+  if (IPV6_REGEX.test(value)) {
     return "ip";
   }
   if (HASH_REGEX.test(value)) {
